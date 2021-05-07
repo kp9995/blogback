ac39316f502df5ba79bb9b6daf71598c5622a310
Kishan Patel <kishanpatelofficial@gmail.com>
2021-05-07 22:20:37 +0530

    blog app uploaded

---
+++ blog/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
+
+# Create your models here.
+
+
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (('published', 'Published'), ('draft', 'Draft'))
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug
+                                                 ])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=85)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'comment by{self.name} on{self.post}'
---
+++ blog/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+# from django.views.generic import ListView
+from taggit.models import Tag
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.core.mail import send_mail
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, TrigramSimilarity
+
+# Create your views here.
+
+
+def post_list(request, tag_slug=None):
+    # queryset = Post.published.all()
+    # context_object_name = 'posts'
+    # paginate_by = 3
+    # template_name = 'blog/post/list.html'
+
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    context = {'page': page, 'posts': posts, 'tag': tag}
+    return render(request, 'blog/post/list.html', context=context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             )
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        # comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # create comment object but not going to save because need to assign post
+            new_comment = comment_form.save(commit=False)
+            # assign a post to comment
+            new_comment.post = post
+            # saving the new comment to the database
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'new_comment': new_comment,
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
+
+
+def post_share(request, post_id):
+    # now retrieve post with post id
+
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form field is passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you to rea" \
+                      f"d "f"{post.title}"
+            message = f"read{post.title} at {post_url}\n\n"f"{cd['name']}\'s comments :{cd['comments']}"
+            send_mail(subject, message, 'kishanpatelofficial@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query),)\
+            .filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
